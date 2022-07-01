7fecb575ed94c70d12eb01ae9479e0d417851aaa
mohammadsajad Dabbagh <45653004+mdabbagh-ir@users.noreply.github.com>
2022-07-01 16:09:58 +0430

    register Interface

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Strategy\SmsChannels\Drivers\kavengar;
+use App\Strategy\SmsChannels\Drivers\Smsir;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind('App\Strategy\SmsChannels\SmsChannelsInterface', function() {
+            return new kavengar;
+            //return new Smsir;
+        });
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Strategy\SmsChannels\SmsChannelsInterface;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (SmsChannelsInterface $SmsChannelsInterface) {
+    dd($SmsChannelsInterface);
 });
